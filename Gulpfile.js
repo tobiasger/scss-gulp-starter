6730572211c1404118ae377c963b6f97c7a6bd63
Tobias Gerhardsson <tobias.gerhardsson@gmail.com>
2017-09-11 23:40:25 +0200

    add autoprefixer and cleancss

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./css/'))
 });
 
