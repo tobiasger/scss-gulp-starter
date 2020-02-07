92eda8184abab7eb1a0bfd2a087d0b06f753ab5e
Tobias Gerhardsson <tobias.gerhardsson@gmail.com>
2020-02-07 11:48:41 +0100

    fix watch function

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,11 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass'),
-      autoprefixer = require('gulp-autoprefixer'),
-      cleanCSS = require('gulp-clean-css'),
-      browserSync = require('browser-sync').create();
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+function styles(){
+    return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(autoprefixer({
@@ -14,17 +14,17 @@ gulp.task('styles', function() {
         }))
         .pipe(gulp.dest('./css/'))
         .pipe(browserSync.stream());
-});
+}
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss', gulp.series('styles'));
+    gulp.watch('sass/**/*.scss', gulp.series(styles));
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
 gulp.task("sync", gulp.series("default", function(){
-    browserSync.init({
-        server: "./",
+  browserSync.init({
+    server: "./",
         notify: false
     });
 }))
